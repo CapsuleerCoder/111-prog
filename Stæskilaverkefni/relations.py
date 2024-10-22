c4bdf3f1a81043d43eaf9bd3cf396f888eac0fef
CapsuleerCoder <oli0202@gmail.com>
2024-10-22 00:41:43 +0000

    stæverk

---
+++ Stæskilaverkefni/relations.py
@@ -0,0 +1,52 @@
+# T-117-STR1 Discrete Mathematics I
+# Template for Individual assignment 5
+
+# Problem 1a)
+def is_reflexive(defined_set, relation_on_set):
+    for i in defined_set:
+        if (i, i) not in relation_on_set:
+            return False
+    return True
+
+# Problem 1b)
+def is_symmetric(relation_on_set):
+    return #TODO Implement
+
+# Problem 1c)
+def is_antisymmetric(relation_on_set):
+    return #TODO Implement
+
+# Problem 1d)
+def is_transitive(relation_on_set):
+    return #TODO Implement
+
+# Problem 2    
+def is_equivalence_relation(defined_set, relation_on_set):
+    return #TODO Implement
+
+# Problem 3
+def composite_relations(relation1, relation2):
+    return #TODO Implement
+
+# Problem 4a)
+def aces_in_relation_a(A):
+    return #TODO Implement
+
+# Problem 4b)
+def aces_in_relation_b(A):
+    return #TODO Implement
+
+# Problem 4c)
+def aces_in_relation_c(A):
+    return #TODO Implement
+   
+# Problem 4d)
+def aces_in_relation_d(A):
+    return #TODO Implement
+    
+# Problem 5e)
+def aces_in_relation_e(A):
+    return #TODO Implement
+
+
+print (is_reflexive ([1, 2, 3, 4], [(1, 1),(1, 3),(2, 2),(3, 3),(2, 3),(1, 4)]))
